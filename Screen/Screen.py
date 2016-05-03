55766fbc5aff2908e2315ae8bb0b2b1dd26c24f3
Vasja Volin <vasja.volin@gmail.com>
2016-05-03 14:22:31 -0400

    mock screen printer

---
+++ Screen/Screen.py
@@ -0,0 +1,9 @@
+class Screen:
+    width = 80
+    height = 20
+    def printScreen(self, data):
+        for i in range(1, 20):
+            print('12345678901234567890123456789012345678901234567890123456789012345678901234567890')
+
+s = Screen();
+s.printScreen('something');
