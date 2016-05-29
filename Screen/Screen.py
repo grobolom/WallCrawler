2dbebf0cbcbf5186693e735ddaad258c250693c5
Vasja Volin <vasja.volin@gmail.com>
2016-05-29 19:27:22 -0400

    remove screen while we figure out how we want to render stuff

--- Screen/Screen.py
+++
@@ -1,9 +0,0 @@
-class Screen:
-    width = 80
-    height = 20
-    def printScreen(self, data):
-        for i in range(1, 20):
-            print '' for i in range(1, self.width)
-
-s = Screen();
-s.printScreen('something');
