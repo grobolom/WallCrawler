22ac827107a24469906132aefd7a99faf04565ad
Vasja Volin <vasja.volin@gmail.com>
2016-05-29 17:51:14 -0400

    messing around with screen

--- Screen/Screen.py
+++ Screen/Screen.py
@@ -3,7 +3,7 @@ class Screen:
     height = 20
     def printScreen(self, data):
         for i in range(1, 20):
-            print('12345678901234567890123456789012345678901234567890123456789012345678901234567890')
+            print '' for i in range(1, self.width)
 
 s = Screen();
 s.printScreen('something');
