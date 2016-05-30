ce90155d320f87242615014bac53336fad9438e8
Vasja Volin <vasja.volin@gmail.com>
2016-05-29 22:16:16 -0400

    fix drawing stuff in the middle of the screen

--- app/Screen.py
+++ app/Screen.py
@@ -1,21 +1,51 @@
 class Screen:
     def draw(self, obj):
-        center = obj['character_position']
-        map_size = obj['map_size']
+        c_position  = obj['character_position']
+        map_size    = obj['map_size']
+        screen_size = obj['screen_size']
 
-        x_min = max(center[0] - 5, 0)
-        y_min = max(center[1] - 5, 0)
-        x_max = min(center[0] + 5, map_size[0])
-        y_max = min(center[1] + 5, map_size[1])
+        centered = self.centered(obj)
+        if centered:
+            pos = self.centered_pos(obj)
+        else:
+            pos = self.offset_pos(obj)
 
-        x_size = x_max - x_min
-        y_size = y_max - y_min
+        m = [ '.' * screen_size[0] ] * screen_size[1]
 
-        m = [ '.' * x_size ] * y_size
-
-        change = m[center[1]]
-        m[center[1]] = change[:center[0]] + '@' + change[center[0] + 1:]
-
-        print(m)
+        line = m[pos[1]]
+        m[pos[1]] = line[:pos[0]] + '@' + line[pos[0] + 1:]
 
         return m
+
+    def centered_pos(self, obj):
+        screen_size_x = obj['screen_size'][0]
+        screen_size_y = obj['screen_size'][1]
+
+        radius_x = (screen_size_x - 1) / 2
+        radius_y = (screen_size_y - 1) / 2
+
+        return (
+            radius_x,
+            radius_y,
+        )
+
+    def offset_pos(self, obj):
+        pass
+
+    def centered(self, obj):
+        screen_size_x = obj['screen_size'][0]
+        screen_size_y = obj['screen_size'][1]
+
+        position_x = obj['character_position'][0]
+        position_y = obj['character_position'][1]
+
+        radius_x = (screen_size_x - 1) / 2
+        radius_y = (screen_size_y - 1) / 2
+
+        map_end_x = obj['map_size'][0] - 1
+        map_end_y = obj['map_size'][1] - 1
+
+        return  position_x + radius_x <= map_end_x and \
+                position_y + radius_y <= map_end_y and \
+                position_x - radius_x >= 0 and \
+                position_y - radius_y >= 0
--- tests/testScreen.py
+++ tests/testScreen.py
@@ -3,7 +3,7 @@ from app import Screen
 class TestScreen:
     def test_it_should_draw_a_screen_around_the_character(self):
         m = {
-            'character_position': (0, 0),
+            'character_position': (2, 2),
             'map_size':           (10, 10),
             'screen_size':        (5, 5),
         }
@@ -11,28 +11,24 @@ class TestScreen:
 
         result = s.draw(m)
         assert result == [
-            '@....',
             '.....',
             '.....',
+            '..@..',
             '.....',
             '.....',
         ]
 
-    def test_it_should_draw_something_around_a_center_point(self):
+    def test_it_should_draw_the_bottom_right_corner(self):
         m = {
             'character_position': (2, 2),
-            'map_size':           (10, 10),
-            'screen_size':        (7, 7),
+            'map_size':           (4, 4),
+            'screen_size':        (3, 3),
         }
         s = Screen()
 
         result = s.draw(m)
         assert result == [
-            '.......',
-            '.......',
-            '..@....',
-            '.......',
-            '.......',
-            '.......',
-            '.......',
+            '...',
+            '.@.',
+            '...',
         ]
