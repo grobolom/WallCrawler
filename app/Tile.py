7663007d814d3c7963f45578508cabe26eb9455f
Vasja Volin <vasja.volin@gmail.com>
2016-05-30 17:27:17 -0400

    add some map and tile classes

---
+++ app/Tile.py
@@ -0,0 +1,15 @@
+class Tile:
+    def __init__(self):
+        self.walkable = True
+        self.blocks_los = False
+        self.ascii_rep = ''
+
+class Floor(Tile):
+    def __init__(self):
+        super(Floor, self).__init__()
+        self.ascii_rep = '.'
+
+class Wall(Tile):
+    def __init__(self):
+        super(Wall, self).__init__()
+        self.ascci_rep = '#'
