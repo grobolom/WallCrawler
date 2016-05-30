41c4069f979b2dfa6db8e4e609861ccf7ca5c8de
Vasja Volin <vasja.volin@gmail.com>
2016-05-29 20:27:13 -0400

    add drawing to a point elsewhere on the page

--- app/Screen.py
+++ app/Screen.py
@@ -1,5 +1,21 @@
 class Screen:
     def draw(self, obj):
-        return [
-            '@' + '.' * 4,
-        ] + ['.' * 5] * 4
+        center = obj['character_position']
+        map_size = obj['map_size']
+
+        x_min = max(center[0] - 5, 0)
+        y_min = max(center[1] - 5, 0)
+        x_max = min(center[0] + 5, map_size[0])
+        y_max = min(center[1] + 5, map_size[1])
+
+        x_size = x_max - x_min
+        y_size = y_max - y_min
+
+        m = [ '.' * x_size ] * y_size
+
+        change = m[center[1]]
+        m[center[1]] = change[:center[0]] + '@' + change[center[0] + 1:]
+
+        print(m)
+
+        return m
--- tests/testScreen.py
+++ tests/testScreen.py
@@ -4,7 +4,8 @@ class TestScreen:
     def test_it_should_draw_a_screen_around_the_character(self):
         m = {
             'character_position': (0, 0),
-            'map_size': (10, 10),
+            'map_size':           (10, 10),
+            'screen_size':        (5, 5),
         }
         s = Screen()
 
@@ -15,4 +16,23 @@ class TestScreen:
             '.....',
             '.....',
             '.....',
+        ]
+
+    def test_it_should_draw_something_around_a_center_point(self):
+        m = {
+            'character_position': (2, 2),
+            'map_size':           (10, 10),
+            'screen_size':        (7, 7),
+        }
+        s = Screen()
+
+        result = s.draw(m)
+        assert result == [
+            '.......',
+            '.......',
+            '..@....',
+            '.......',
+            '.......',
+            '.......',
+            '.......',
         ]
