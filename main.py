757f1bd360d8dfa5dc033b5cc0e0826585bd24db
Vasja Volin <vasja.volin@gmail.com>
2016-05-30 18:33:32 -0400

    fix manual testing to work with new structure

--- main.py
+++ main.py
@@ -2,10 +2,19 @@ import app
 import blessed
 
 def main():
+    s_map = {
+        'tiles': [ [ app.Floor() for i in range(10) ] for j in range(10) ],
+        'size': [10, 10]
+    }
+    s_map['tiles'][2][2] = app.Wall()
+    s_map['tiles'][7][7] = app.Wall()
+    s_map['tiles'][7][2] = app.Wall()
+    s_map['tiles'][2][7] = app.Wall()
+
     state = {
         'character_position': [0, 0],
-        'map_size': [5, 5],
-        'screen_size': [3, 3],
+        'screen_size': [5, 5],
+        'map': s_map,
     }
 
     s = app.Screen()
@@ -21,7 +30,10 @@ def main():
 
         action = k.getAction(key)
         new_state = r.reduce(new_state, action)
-        print('\n'. join(s.draw(new_state)))
+
+        for l in s.draw(new_state):
+            print(''.join(l))
+
         print(state['character_position'])
 
 
