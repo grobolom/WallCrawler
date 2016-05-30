b70ffe895b2d543ce5534cd5f48b52be74bc4edc
Vasja Volin <vasja.volin@gmail.com>
2016-05-29 23:03:42 -0400

    add tests for moving up and down

--- tests/testPositionReducer.py
+++ tests/testPositionReducer.py
@@ -12,13 +12,47 @@ class TestPositionReducer:
 
         assert new_state['character_position'] == [5, 4]
 
-    def test_it_should_not_move_past_the_end_of_the_map(self):
+    def test_it_should_not_move_past_the_top_of_the_map(self):
         reducer = PositionReducer()
         state = {
-            'character_position': [5, 0],
+            'character_position': [0, 0],
             'map_size': [6, 6],
         }
         action = { 'name': 'MOVE_UP' }
         new_state = reducer.reduce(state, action)
 
-        assert new_state['character_position'] == [5, 0]
+        assert new_state['character_position'] == [0, 0]
+
+    def test_it_should_not_move_past_the_bottom_of_the_map(self):
+        reducer = PositionReducer()
+        state = {
+            'character_position': [5, 5],
+            'map_size': [6, 6],
+        }
+        action = { 'name': 'MOVE_DOWN' }
+        new_state = reducer.reduce(state, action)
+
+        assert new_state['character_position'] == [5, 5]
+
+
+    def test_it_should_not_move_past_the_left_of_the_map(self):
+        reducer = PositionReducer()
+        state = {
+            'character_position': [0, 0],
+            'map_size': [6, 6],
+        }
+        action = { 'name': 'MOVE_LEFT' }
+        new_state = reducer.reduce(state, action)
+
+        assert new_state['character_position'] == [0, 0]
+
+    def test_it_should_not_move_past_the_right_of_the_map(self):
+        reducer = PositionReducer()
+        state = {
+            'character_position': [5, 5],
+            'map_size': [6, 6],
+        }
+        action = { 'name': 'MOVE_RIGHT' }
+        new_state = reducer.reduce(state, action)
+
+        assert new_state['character_position'] == [5, 5]
