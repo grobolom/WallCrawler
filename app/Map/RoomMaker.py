8e07e513b62fcd1238bc4312c2c9b40530c4fdc7
Vasja Volin <vasja.volin@gmail.com>
2016-05-30 23:43:20 -0400

    add code for raising exceptions when map is off edge

--- app/Map/RoomMaker.py
+++ app/Map/RoomMaker.py
@@ -4,6 +4,9 @@ class RoomMaker:
     def addRoom(self, map, room_corner, room_size):
         new_map = map
 
+        if self._roomGoesPastMapEdge(map['size'], room_corner, room_size):
+            raise Exception('room too big')
+
         tiles = map['tiles']
         for x in range(room_size[0]):
             for y in range(room_size[1]):
@@ -12,3 +15,7 @@ class RoomMaker:
         new_map['tiles'] = tiles
 
         return new_map
+
+    def _roomGoesPastMapEdge(self, map_size, room_corner, room_size):
+        return room_corner[0] + room_size[0] - 1 > map_size[0] or \
+               room_corner[1] + room_size[1] - 1 > map_size[1]
