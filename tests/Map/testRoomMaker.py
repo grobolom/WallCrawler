88884a4ec95ce25087d83bff0ef3fed1a2cc6a45
Vasja Volin <vasja.volin@gmail.com>
2016-05-30 23:23:26 -0400

    add a failing test for adding a room

--- tests/Map/testRoomMaker.py
+++ tests/Map/testRoomMaker.py
@@ -1,2 +1,17 @@
+from app.Map import RoomMaker
+from app import Tile
+
 class TestRoomMaker:
-    pass
+    def test_it_should_make_a_room(self):
+        rooms = []
+        map = {
+            'tiles': [[ None for i in range(80) ] for j in range(20) ],
+            'size': [80, 20]
+        }
+        room_corner = (4, 10)
+        room_size = (5, 5)
+
+        r = RoomMaker()
+        map = r.addRoom(map, room_corner, room_size)
+
+        assert type(map['tiles'][15][9]) == Tile
