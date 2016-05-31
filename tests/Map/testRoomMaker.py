c17e70253096fc5d65036f57b826ae0e6469c7ce
Vasja Volin <vasja.volin@gmail.com>
2016-05-31 00:04:42 -0400

    add test for writing over Tile() objects

--- tests/Map/testRoomMaker.py
+++ tests/Map/testRoomMaker.py
@@ -1,7 +1,7 @@
 import unittest
 
+import app
 from app.Map import RoomMaker
-from app import Floor
 
 class TestRoomMaker(unittest.TestCase):
     def test_it_should_make_a_room(self):
@@ -15,7 +15,7 @@ class TestRoomMaker(unittest.TestCase):
         r = RoomMaker()
         map = r.addRoom(map, room_corner, room_size)
 
-        assert type(map['tiles'][8][8]) == Floor
+        assert type(map['tiles'][8][8]) == app.Floor
 
     def test_it_should_not_add_a_room_off_the_edge_of_the_map(self):
         map = {
@@ -40,3 +40,16 @@ class TestRoomMaker(unittest.TestCase):
         r.addRoom(map, (0, 0), (5, 5))
         with self.assertRaises(Exception) as context:
             r.addRoom(map, (2, 2), (7, 7))
+
+    def test_it_should_be_ok_overwriting_tiles(self):
+        map = {
+            'tiles': [[ app.Tile for i in range(80) ] for j in range(20) ],
+            'size': [80, 20]
+        }
+        room_corner = (4, 4)
+        room_size = (5, 5)
+
+        r = RoomMaker()
+        map = r.addRoom(map, room_corner, room_size)
+
+        assert type(map['tiles'][8][8]) == app.Floor
