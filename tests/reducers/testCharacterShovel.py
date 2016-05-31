f2c814585eff72c0246a0bdcb41b91ab5b0af944
Vasja Volin <vasja.volin@gmail.com>
2016-05-31 11:16:32 -0400

    add a more complete test for shoveling the character to the object list

--- tests/reducers/testCharacterShovel.py
+++ tests/reducers/testCharacterShovel.py
@@ -3,14 +3,15 @@ import app
 class TestCharacterShovel:
     def test_it_should_shovel_the_character_into_the_list_of_objects(self):
         c = app.Character()
-        objects = []
+        someObject = app.Object()
+        objects = [ someObject ]
 
         state = {
             'objects': objects,
             'character': c,
         }
 
-        expected = [ c ]
+        expected = [ someObject, c ]
 
         reducer = app.reducers.CharacterShovel()
         new_state = reducer.reduce(state, {})
