db920f445d4843ff14455ea4bc22b5a329a94c58
Vasja Volin <vasja.volin@gmail.com>
2016-05-30 23:43:33 -0400

    fix test to use assertRaises context manager

--- tests/Map/testRoomMaker.py
+++ tests/Map/testRoomMaker.py
@@ -29,4 +29,5 @@ class TestRoomMaker(unittest.TestCase):
 
         r = RoomMaker()
 
-        self.assertRaises(Exception, r.addRoom(map, room_corner, room_size))
+        with self.assertRaises(Exception) as context:
+            r.addRoom(map, room_corner, room_size)
