313e2c5be55d8729d245e75dff8ce0140526a467
Vasja Volin <vasja.volin@gmail.com>
2016-05-30 20:37:44 -0400

    add a failing basic test for mapview

--- tests/testMapView.py
+++ tests/testMapView.py
@@ -1,2 +1,15 @@
+from app import Tile
+from app import MapView
+
 class TestMapView:
-    pass
+    def test_it_should_draw_a_tile(self):
+        map_view = MapView()
+
+        tile = Tile()
+        tile.ascii_rep = 'x'
+
+        map = [[ tile ]]
+        positions = [[(0, 0)]]
+
+        expected = [['x']]
+        assert expected == map_view.draw(map, positions)
