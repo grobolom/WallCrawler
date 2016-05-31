ec2b27336cd3666d07852fbbcdff5b129726c5e3
Vasja Volin <vasja.volin@gmail.com>
2016-05-30 21:52:54 -0400

    add a test for drawing objects in the map view

--- tests/testMapView.py
+++ tests/testMapView.py
@@ -1,6 +1,12 @@
 from app import Tile
 from app import MapView
 
+class MockObject(object):
+    def __init__(self):
+        self.x = 1
+        self.y = 1
+        self.ascii_rep = '@'
+
 class TestMapView:
     def test_it_should_draw_a_tile(self):
         map_view = MapView()
@@ -25,3 +31,18 @@ class TestMapView:
 
         expected = [['y', 'y']]
         assert expected == map_view.draw(map, positions)
+
+    def test_it_should_draw_objects_on_top_of_tiles(self):
+        obj = MockObject()
+        objects = [ obj ]
+
+        map_view = MapView()
+
+        tile = Tile()
+        tile.ascii_rep = '.'
+        map = [[ tile, tile ],[ tile, tile ]]
+        positions = [[(1, 1)]]
+
+        expected = [['@']]
+
+        assert expected == map_view.draw(map, positions, objects)
