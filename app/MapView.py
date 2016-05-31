6711a2a40bd251b1950c80db278a78eb2abb4195
Vasja Volin <vasja.volin@gmail.com>
2016-05-30 20:33:51 -0400

    add a stub class for drawing tiles

---
+++ app/MapView.py
@@ -0,0 +1,2 @@
+class MapView:
+    pass
--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,5 @@
 from .Screen import Screen
+from .MapView import MapView
 from .PositionReducer import PositionReducer
 from .KeyboardActionBuilder import KeyboardActionBuilder
 
