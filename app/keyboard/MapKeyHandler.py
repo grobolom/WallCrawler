dcf5d5cfd7efc0cf426813816a66ca1e5959cb09
Vasja Volin <vasja.volin@gmail.com>
2016-05-31 20:16:47 -0400

    add code for moving the character in cardinal directions

--- app/keyboard/MapKeyHandler.py
+++ app/keyboard/MapKeyHandler.py
@@ -2,4 +2,27 @@ import app
 
 class MapKeyHandler:
     def getAction(self, key, state):
-        return {}
+        if key not in [ 'h', 'j', 'k', 'l' ]:
+            return None
+
+        to = self._getTo(key, state['character']['position'])
+
+        return {
+            'name': 'MOVE_CHARACTER',
+            'to': to
+        }
+
+    def _getTo(self, key, current_position):
+        (x, y) = current_position
+
+        if key == 'h':
+            return [x - 1, y]
+        if key == 'l':
+            return [x + 1, y]
+
+        if key == 'j':
+            return [x, y + 1]
+        if key == 'k':
+            return [x, y - 1]
+
+        return current_position
