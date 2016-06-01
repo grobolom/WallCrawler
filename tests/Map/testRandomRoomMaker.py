19bd3e5867507ae2a863e2986d8b49a0108139d5
Vasja Volin <vasja.volin@gmail.com>
2016-05-31 22:33:52 -0400

    add a failing test for makign a random room

--- tests/Map/testRandomRoomMaker.py
+++ tests/Map/testRandomRoomMaker.py
@@ -2,4 +2,10 @@ import app
 from app.Map import RandomRoomMaker
 
 class TestRandomRoomMaker:
-    pass
+    def test_it_should_make_a_room_of_random_size(self):
+        sut = RandomRoomMaker()
+
+        result = sut.getRandomRoom(5, 30)
+
+        assert result[0] >= 5 and result[0] <= 30
+        assert result[1] >= 5 and result[1] <= 30
