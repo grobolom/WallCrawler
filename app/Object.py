270bc5938f1344973c171e65b70c1f38c0a7c15b
Vasja Volin <vasja.volin@gmail.com>
2016-06-01 18:37:34 -0400

    add args and kwargs handling to the object class

--- app/Object.py
+++ app/Object.py
@@ -1,6 +1,13 @@
 class Object(object):
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         self.x = 0
         self.y = 0
         self.ascii_rep = '@'
         self.type = 'Object'
+
+        for dictionary in args:
+            for key in dictionary:
+                setattr(self, key, dictionary[key])
+
+        for key in kwargs:
+            setattr(self, key, kwargs[key])
