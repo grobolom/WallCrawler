2b1982f706fe964386f51db6457153220a2d89fe
Vasja Volin <vasja.volin@gmail.com>
2016-06-01 18:37:48 -0400

    make sure the tile picked doesn't pick occupied tiles

--- app/Map/ValidTilePicker.py
+++ app/Map/ValidTilePicker.py
@@ -5,15 +5,24 @@ class ValidTilePicker:
     def getRandomEmptyFloorTile(self, map):
         tiles    = map['tiles']
         map_size = map['size']
+        objects  = map['objects']
 
         found = False
         while not found:
             x = random.randint(0, map_size[0] - 1)
             y = random.randint(0, map_size[1] - 1)
+            pos = [x, y]
 
-            if type(tiles[y][x]) == app.Floor:
-                found = True
+            if type(tiles[y][x]) == app.Floor and \
+               self.tileDoesNotHaveAnObjectOnIt(pos, objects):
+
                 return {
-                    'pos': [x, y],
+                    'pos': pos,
                     'tile': tiles[y][x],
                 }
+
+    def tileDoesNotHaveAnObjectOnIt(self, pos, objects):
+        for obj in objects:
+            if obj.position == pos:
+                return False
+        return True
--- tests/Map/testValidTilePicker.py
+++ tests/Map/testValidTilePicker.py
@@ -2,14 +2,26 @@ import app
 from app.Map import ValidTilePicker
 
 class TestValidTilePicker:
-    def test_it_should_pick_a_random_empty_floor_tile(self):
+    def test_it_should_pick_a_random_floor_tile(self):
         sut = ValidTilePicker()
 
         map = {
             'tiles': [[ app.Wall(), app.Floor() ]],
             'size': [2, 1],
+            'objects': [],
         }
         tile = sut.getRandomEmptyFloorTile(map)
 
         assert tile['pos'] == [1, 0]
 
+    def test_it_should_not_pick_a_tile_with_an_object_on_it(self):
+        sut = ValidTilePicker()
+
+        map = {
+            'tiles': [[ app.Floor(), app.Floor() ]],
+            'size': [2, 1],
+            'objects': [ app.Object(position = [0,0]) ]
+        }
+        tile = sut.getRandomEmptyFloorTile(map)
+
+        assert tile['pos'] == [1, 0]
