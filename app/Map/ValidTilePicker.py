566cf71116651bc20432568c8c7d01daecdd91b6
Vasja Volin <vasja.volin@gmail.com>
2016-06-01 18:13:14 -0400

    add code for picking a random tile

--- app/Map/ValidTilePicker.py
+++ app/Map/ValidTilePicker.py
@@ -1,2 +1,19 @@
+import random
+import app
+
 class ValidTilePicker:
-    pass
+    def getRandomEmptyFloorTile(self, map):
+        tiles    = map['tiles']
+        map_size = map['size']
+
+        found = False
+        while not found:
+            x = random.randint(0, map_size[0] - 1)
+            y = random.randint(0, map_size[1] - 1)
+
+            if type(tiles[y][x]) == app.Floor:
+                found = True
+                return {
+                    'pos': [x, y],
+                    'tile': tiles[y][x],
+                }
--- tests/Map/testValidTilePicker.py
+++ tests/Map/testValidTilePicker.py
@@ -1,4 +1,15 @@
+import app
 from app.Map import ValidTilePicker
 
-class ValidTilePicker:
-    pass
+class TestValidTilePicker:
+    def test_it_should_pick_a_random_empty_floor_tile(self):
+        sut = ValidTilePicker()
+
+        map = {
+            'tiles': [[ app.Wall(), app.Floor() ]],
+            'size': [2, 1],
+        }
+        tile = sut.getRandomEmptyFloorTile(map)
+
+        assert tile['pos'] == [1, 0]
+
