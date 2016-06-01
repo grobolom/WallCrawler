77ff596ef3152d566f744dfa2f3f5160f97007f0
Vasja Volin <vasja.volin@gmail.com>
2016-05-31 23:05:00 -0400

    add code to check if a tile is empty

--- app/Map/MapGenerator.py
+++ app/Map/MapGenerator.py
@@ -1,2 +1,17 @@
+import app
+
 class MapGenerator:
-    pass
+    def getMap(self, rooms, min_room_size, max_rooms_size, map_size):
+        pass
+
+    def findValidPlacementSpot(self, room_size, map):
+        random_square = self.getRandomMapSquare(map)
+
+        return self.isEmpty(random_square, map) and \
+               self.hasANotEmptySquareNearby(random_square, map) and \
+               self.roomWouldFit(random_square, map)
+
+    def isEmpty(self, pos, map):
+        x = pos[0]
+        y = pos[1]
+        return map[y][x] == None or type(map[y][x]) == app.Tile
