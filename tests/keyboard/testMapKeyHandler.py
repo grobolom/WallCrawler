f78e30cd17e88ce8862f08fb758cd8290ec2cd9e
Vasja Volin <vasja.volin@gmail.com>
2016-05-31 21:40:23 -0400

    add tests for moving character in the other cardinal directions

--- tests/keyboard/testMapKeyHandler.py
+++ tests/keyboard/testMapKeyHandler.py
@@ -14,3 +14,42 @@ class TestMapKeyHandler:
         }
 
         assert expected == MapKeyHandler().getAction(key, state)
+
+    def test_it_should_try_to_move_the_character_down(self):
+        key = 'j'
+        state = {
+            'character': app.Character({'x': 1, 'y': 1})
+        }
+
+        expected = {
+            'name': 'MOVE_CHARACTER',
+            'to': [1, 2]
+        }
+
+        assert expected == MapKeyHandler().getAction(key, state)
+
+    def test_it_should_try_to_move_the_character_left(self):
+        key = 'h'
+        state = {
+            'character': app.Character({'x': 1, 'y': 1})
+        }
+
+        expected = {
+            'name': 'MOVE_CHARACTER',
+            'to': [0, 1]
+        }
+
+        assert expected == MapKeyHandler().getAction(key, state)
+
+    def test_it_should_try_to_move_the_character_right(self):
+        key = 'l'
+        state = {
+            'character': app.Character({'x': 1, 'y': 1})
+        }
+
+        expected = {
+            'name': 'MOVE_CHARACTER',
+            'to': [2, 1]
+        }
+
+        assert expected == MapKeyHandler().getAction(key, state)
