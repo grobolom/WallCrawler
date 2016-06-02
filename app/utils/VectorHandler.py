2f21e5244a76ab6a42a23e68e581c5b7aaa6c125
Vasja Volin <vasja.volin@gmail.com>
2016-06-01 21:52:14 -0400

    add a basic case for positive and negative vectors

--- app/utils/VectorHandler.py
+++ app/utils/VectorHandler.py
@@ -1,2 +1,13 @@
 class VectorHandler:
-    pass
+    """
+    this class if for handling vectors, especially in making maps.
+    """
+    def getPositiveVectors(self, v1, v2):
+        """
+        this is used in making rooms - it's easy to generate a room start
+        point and a random vector representing the size but for ease of use we
+        would rather both the position AND the size be positive
+        """
+
+        return ([2, 2], [3, 3])
+  
--- tests/utils/testVectorHandler.py
+++ tests/utils/testVectorHandler.py
@@ -1,2 +1,11 @@
+from app.utils import VectorHandler
+
 class TestVectorHandler:
-    pass
+    def test_it_should_swap_a_positive_and_negative_vector_to_positive(self):
+        v1 = [5, 5]
+        v2 = [-3, -3]
+
+        first, second = VectorHandler().getPositiveVectors(v1, v2)
+
+        assert first == [2, 2]
+        assert second == [3, 3]
