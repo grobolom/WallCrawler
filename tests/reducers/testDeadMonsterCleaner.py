3f0efece2d35624c7b21c5a58a5c5cfcb97ef3c0
Vasja Volin <vasja.volin@gmail.com>
2016-06-03 15:23:22 -0400

    add a test for covering removing negative hp monsters

--- tests/reducers/testDeadMonsterCleaner.py
+++ tests/reducers/testDeadMonsterCleaner.py
@@ -14,3 +14,20 @@ class TestDeadMonsterCleaner:
         new_state = sut.reduce(state, action)
 
         assert new_state['objects'] == []
+
+    def test_it_should_remove_all_monsters_with_less_than_one_health(self):
+        sut = DeadMonsterCleaner()
+
+        dead_monster = app.Monster(id = 1, hp = 0)
+        live_monster = app.Monster(id = 2, hp = 10)
+        superdead_monster = app.Monster(id = 3, hp = -5)
+
+        action = {}
+        state = {
+            'objects': [ dead_monster, live_monster, superdead_monster ]
+        }
+
+        new_state = sut.reduce(state, action)
+
+        assert len(new_state['objects']) == 1
+        assert new_state['objects'][0].id == 2
