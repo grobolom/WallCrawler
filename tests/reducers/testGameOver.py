39a61ec335dd910f1e60c7fff3ab84da7690b71c
Vasja Volin <vasja.volin@gmail.com>
2016-06-03 16:55:48 -0400

    add a test to cover game over actions

--- tests/reducers/testGameOver.py
+++ tests/reducers/testGameOver.py
@@ -1,4 +1,12 @@
 from app.reducers import GameOver
 
 class TestGameOver:
-    pass
+    def it_should_end_the_game_when_the_action_is_game_over(self):
+        sut = GameOver()
+
+        action = { 'name': 'GAME_OVER' }
+        state = {}
+
+        new_state = sut.reduce(state, action)
+
+        assert new_state['game_over'] == True
