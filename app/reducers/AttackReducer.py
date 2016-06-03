d1945b3dad3deaa0b4d4bb1a40eb16900c650e8b
Vasja Volin <vasja.volin@gmail.com>
2016-06-03 14:52:13 -0400

    add some simple code to cover base test case

--- app/reducers/AttackReducer.py
+++ app/reducers/AttackReducer.py
@@ -2,4 +2,8 @@ class AttackReducer:
     def reduce(self, action, state):
         if action['name'] != 'ATTACK_CHARACTER':
             return state
-        return state
+
+        new_state = state
+        new_state['objects'][0].hp -= 1
+
+        return new_state
