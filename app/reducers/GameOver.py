521b24bc6641942f2f2554b1bdd6f21c7a3bf9ce
Vasja Volin <vasja.volin@gmail.com>
2016-06-03 17:00:47 -0400

    cover ending the game on game over action

--- app/reducers/GameOver.py
+++ app/reducers/GameOver.py
@@ -1,3 +1,6 @@
 class GameOver:
     def reduce(self, state, action):
-        pass
+        new_state = state
+        if action['name'] == 'GAME_OVER':
+            new_state['game_over'] = True
+        return new_state
