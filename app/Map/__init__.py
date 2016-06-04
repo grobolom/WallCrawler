4ff0edcfcc1f784f03f62f9d3f69116ba720d75a
Vasja Volin <vasja.volin@gmail.com>
2016-06-04 19:56:21 -0400

    add a stub for a better shadow casting renderer

--- app/Map/__init__.py
+++ app/Map/__init__.py
@@ -4,3 +4,4 @@ from .MapGenerator import MapGenerator
 from .ValidTilePicker import ValidTilePicker 
 
 from .ShadowCaster import ShadowCaster
+from .LightCaster import LightCaster
---
+++ tests/Map/testLightCaster.py
@@ -0,0 +1,5 @@
+import app
+from app.Map import LightCaster
+
+class TestShadowCaster:
+    pass
