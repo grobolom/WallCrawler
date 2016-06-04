8b77b58e8e29b031982b83f1719decac0c285909
Vasja Volin <vasja.volin@gmail.com>
2016-06-03 22:41:24 -0400

    fix xp not being assigned to the characters

--- tests/reducers/testDeadMonsterCleaner.py
+++ tests/reducers/testDeadMonsterCleaner.py
@@ -5,13 +5,15 @@ class TestDeadMonsterCleaner:
     def test_it_should_remove_all_dead_monsters(self):
         sut = DeadMonsterCleaner()
 
+        character = app.Character(id = 2, dead = True)
         dead_monster = app.Monster(id = 1, hp = 0, dead = True)
         live_monster = app.Monster(id = 2, hp = 10, dead = False)
         dead_with_hp = app.Monster(id = 3, hp = -5, dead = True)
 
         action = {}
         state = {
-            'objects': [ dead_monster, live_monster, dead_with_hp ]
+            'character': character,
+            'objects': [ dead_monster, live_monster, dead_with_hp ],
         }
 
         new_state = sut.reduce(state, action)
@@ -27,6 +29,7 @@ class TestDeadMonsterCleaner:
 
         action = {}
         state = {
+            'character': character,
             'objects': [ dead_monster, character ]
         }
 
@@ -35,7 +38,7 @@ class TestDeadMonsterCleaner:
         assert len(new_state['objects']) == 1
         assert new_state['objects'][0].id == 2
 
-    def test_it_should_give_xp_to_killers(self):
+    def test_it_should_give_dead_monster_xp_to_the_character(self):
         sut = DeadMonsterCleaner()
 
         character = app.Character(id = 2, xp = 2)
@@ -43,10 +46,10 @@ class TestDeadMonsterCleaner:
 
         action = {}
         state = {
-            'objects': [ character ]
+            'character': character,
+            'objects': [ monster, character ],
         }
 
         new_state = sut.reduce(state, action)
 
-        assert(len(new_state['objects'])) == 1
-        assert new_state['objects'][0].xp == 5
+        assert new_state['character'].xp == 5
