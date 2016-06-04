6a1b064c541427bfd98e2e20b62d493a8377bd89
Vasja Volin <vasja.volin@gmail.com>
2016-06-03 23:55:39 -0400

    add a test for polling monster actions

--- app/monsters/testMonsterActionHandler.py
+++ app/monsters/testMonsterActionHandler.py
@@ -1,5 +1,22 @@
 import app
 from app.monsters import MonsterActionHandler
 
+from mock import MagicMock as Mock
+
 class TestMonsterActionHandler:
-    pass
+    def test_it_should_get_an_action_from_every_monster(self):
+        monster1 = app.Monster(id = 1)
+        monster2 = app.Monster(id = 2)
+
+        monster1.getAction = Mock()
+        monster2.getAction = Mock()
+
+        state = {
+            'objects': [ monster1, monster2 ],
+        }
+        actions = MonsterActionHandler.getMonsterActions(state)
+
+        assert len(actions) == 2
+
+        assert monster1.getAction.called_once_with(state)
+        assert monster2.getAction.called_once_with(state)
