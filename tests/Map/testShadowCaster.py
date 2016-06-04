68afc12fdc9a467a3b9bce90d248c25f72cf6992
Vasja Volin <vasja.volin@gmail.com>
2016-06-04 14:13:47 -0400

    add some simpler test for shadowcasting

--- tests/Map/testShadowCaster.py
+++ tests/Map/testShadowCaster.py
@@ -1,6 +1,39 @@
 from app.Map import ShadowCaster
 
 class TestShadowCaster:
+    def test_it_should_draw_a_super_small_shadow(self):
+        octet = [
+            '   .',
+            '  ..',
+            ' ...',
+            '@.#.',
+        ]
+        result = [
+            '   .',
+            '  ..',
+            ' ...',
+            '@.#.',
+        ]
+        sut = ShadowCaster()
+        assert sut.getShadowMap(octet) == result
+
+    def test_it_should_cast_some_small_shadows(self):
+        octet = [
+            '     .',
+            '    ..',
+            '   #..',
+            '  ...#',
+            ' .....',
+            '@...#.',
+        ]
+        result = [
+            '     s',
+            '    s.',
+            '   #..',
+            '  ...#',
+            ' .....',
+            '@...#s',
+        ]
     def test_it_should_cast_some_shadows(self):
         sut = ShadowCaster()
         octet = [
