f58013b748d4830bd63375ad3b08b8cb5eea0ba1
Vasja Volin <vasja.volin@gmail.com>
2016-06-04 14:31:06 -0400

    add code for just hitting all the squares on the map

--- app/Map/ShadowCaster.py
+++ app/Map/ShadowCaster.py
@@ -1,2 +1,18 @@
 class ShadowCaster:
-    pass
+    def getShadowMap(self, octet):
+        height = len(octet)
+        width = len(octet[0])
+
+        result = [[
+            ' ' for col in range(width) ]
+                for row in range(height) ]
+
+        for x in range(width):
+            for y in range(height):
+                if octet[y][x] == ' ':
+                    result[y][x] = ' '
+                    continue
+                result[y][x] = 'x'
+
+        print( [ ''.join(row) for row in result ] )
+        return [ ''.join(row) for row in result ]
