20d926aa84c951e9579333bb1097b57a0af2928f
Vasja Volin <vasja.volin@gmail.com>
2016-06-03 23:29:40 -0400

    add code to handle moving a monster by ID

--- app/reducers/MonsterMover.py
+++ app/reducers/MonsterMover.py
@@ -1,3 +1,18 @@
 class MonsterMover:
     def reduce(self, state, action):
-        return state
+        new_state = state
+
+        if action['name'] != 'MOVE':
+            return new_state
+
+        id = action['target']
+        to = action['to']
+
+        new_objects = state['objects']
+        for key, o in enumerate(new_objects):
+            if o.id == id:
+                new_objects[key].position = to
+                break
+        state['objects'] = new_objects
+
+        return new_state
