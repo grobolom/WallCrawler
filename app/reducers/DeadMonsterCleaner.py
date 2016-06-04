34169049115a3a5eb60a851396a2cc86f1f82ff7
Vasja Volin <vasja.volin@gmail.com>
2016-06-03 23:03:43 -0400

    refactor dead monster cleaner to no longer assign monster xp

--- app/reducers/DeadMonsterCleaner.py
+++ app/reducers/DeadMonsterCleaner.py
@@ -3,24 +3,9 @@ class DeadMonsterCleaner:
         new_state = state
 
         objects = new_state['objects']
-        character = new_state['character']
-
-        gained_xp = self.getGainedExp(objects)
-
-        character.xp += gained_xp
-
         new_objects = [
             o for o in objects if not (o.type == 'monster' and o.isDead())
         ]
-
         new_state['objects'] = new_objects
-        new_state['character'] = character
 
         return new_state
-
-    def getGainedExp(self, objects):
-        xp = 0
-        for o in objects:
-            if o.type == 'monster' and o.isDead():
-                xp += o.xp
-        return xp
--- tests/reducers/testDeadMonsterCleaner.py
+++ tests/reducers/testDeadMonsterCleaner.py
@@ -5,18 +5,15 @@ class TestDeadMonsterCleaner:
     def test_it_should_remove_all_dead_monsters(self):
         sut = DeadMonsterCleaner()
 
-        character = app.Character(id = 2, dead = True)
         dead_monster = app.Monster(id = 1, hp = 0, dead = True)
         live_monster = app.Monster(id = 2, hp = 10, dead = False)
         dead_with_hp = app.Monster(id = 3, hp = -5, dead = True)
 
-        action = {}
         state = {
-            'character': character,
             'objects': [ dead_monster, live_monster, dead_with_hp ],
         }
 
-        new_state = sut.reduce(state, action)
+        new_state = sut.reduce(state, {})
 
         assert len(new_state['objects']) == 1
         assert new_state['objects'][0].id == 2
@@ -27,29 +24,11 @@ class TestDeadMonsterCleaner:
         dead_monster = app.Monster(id = 1, dead = True)
         character = app.Character(id = 2, dead = True)
 
-        action = {}
         state = {
-            'character': character,
             'objects': [ dead_monster, character ]
         }
 
-        new_state = sut.reduce(state, action)
+        new_state = sut.reduce(state, {})
 
         assert len(new_state['objects']) == 1
         assert new_state['objects'][0].id == 2
-
-    def test_it_should_give_dead_monster_xp_to_the_character(self):
-        sut = DeadMonsterCleaner()
-
-        character = app.Character(id = 2, xp = 2)
-        monster = app.Monster(xp = 3, id = 1, dead = True, source = character)
-
-        action = {}
-        state = {
-            'character': character,
-            'objects': [ monster, character ],
-        }
-
-        new_state = sut.reduce(state, action)
-
-        assert new_state['character'].xp == 5
