8c24dbfd16e1a2dcfd634f8547bbf1e8fb9d140f
Vasja Volin <vasja.volin@gmail.com>
2016-06-03 23:02:36 -0400

    add code for cleaning up xp

--- app/reducers/XpAssigner.py
+++ app/reducers/XpAssigner.py
@@ -1,3 +1,12 @@
 class XpAssigner:
     def reduce(self, state, action):
-        return state
+        new_state = state
+
+        xp = 0
+        for o in state['objects']:
+            if o.type == 'monster' and o.isDead():
+                xp += o.xp
+
+        new_state['character'].xp += xp
+
+        return new_state
