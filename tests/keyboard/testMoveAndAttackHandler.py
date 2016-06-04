a316ddc5a017454a332436e74a7b24dde2abb679
Vasja Volin <vasja.volin@gmail.com>
2016-06-03 22:51:19 -0400

    cover character movement a bit better

--- tests/keyboard/testMoveAndAttackHandler.py
+++ tests/keyboard/testMoveAndAttackHandler.py
@@ -26,3 +26,26 @@ class TestMoveAndAttackHandler:
         }
 
         assert sut.getAction(key, state) == expected
+
+    def test_it_should_move_if_the_square_is_empty(self):
+        character = app.Character(position=[0, 0])
+
+        state = {
+            'map': {
+                'tiles': [[ app.Floor(), app.Floor() ]],
+                'size': [2, 1],
+            },
+            'character': character,
+            'objects': [ character ],
+        }
+
+        key = 'l'
+
+        sut = MoveAndAttackHandler()
+
+        expected = {
+            'name': 'MOVE_CHARACTER',
+            'to': [1, 0]
+        }
+
+        assert sut.getAction(key, state) == expected
