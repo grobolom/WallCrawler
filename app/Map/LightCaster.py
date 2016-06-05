960998f5bb67f4858a09efecb07bf5c6427a6512
Vasja Volin <vasja.volin@gmail.com>
2016-06-04 20:22:10 -0400

    mess around with implementing a single row light caster

--- app/Map/LightCaster.py
+++ app/Map/LightCaster.py
@@ -1,2 +1,61 @@
 class LightCaster:
-    pass
+    def getShadowedSquares(self, squares, sectors):
+        max_y = len(squares) - 1
+        result = []
+
+        sector_index = 0
+        sector = sectors[sector_index]
+
+        for i, square in enumerate(squares):
+            y = max_y - i
+            pos = [max_y, y]
+
+            if self.squarePastSector(pos, sector):
+                sector_index += 1
+
+            if sector_index > len(sectors) - 1:
+                result += ['s']
+                continue
+
+            sector = sectors[sector_index]
+
+            if self.squareInSector(pos, sector):
+                result += ['.']
+            else:
+                result += ['s']
+
+        return result
+
+    def squarePastSector(self, square, sector):
+        x = float(square[0])
+        y = float(square[1])
+
+        nw_slope = (y + 0.5) / (x - 0.5)
+
+        bottom_slope = sector[1]
+
+        print(square, sector, nw_slope, bottom_slope)
+
+        return nw_slope < bottom_slope
+
+    def squareInSector(self, square, sector):
+        x = float(square[0])
+        y = float(square[1])
+
+        center_slope = y / x
+        se_slope = (y - 0.5) / (x + 0.5)
+        nw_slope = (y + 0.5) / (x - 0.5)
+
+        top_slope = sector[0]
+        bottom_slope = sector[1]
+
+        if center_slope <= top_slope and center_slope >= bottom_slope:
+            return True
+
+        if se_slope < top_slope and se_slope >= bottom_slope:
+            return True
+
+        if nw_slope <= top_slope and nw_slope > bottom_slope:
+            return True
+
+        return False
--- tests/Map/testLightCaster.py
+++ tests/Map/testLightCaster.py
@@ -2,4 +2,22 @@ import app
 from app.Map import LightCaster
 
 class TestShadowCaster:
-    pass
+    def test_it_should_return_what_squares_in_a_row_should_be_shadowed(self):
+        sut = LightCaster()
+
+        squares = ['.', '.', '.']
+        sectors = [ [1.0, 1.0 / 2.0] ]
+
+        result = sut.getShadowedSquares(squares, sectors)
+
+        assert result == ['.', '.', 's']
+
+    def test_it_should_return_the_right_squres_with_multiple_sectors(self):
+        sut = LightCaster()
+
+        squares = ['.', '.', '.', '.']
+        sectors = [ [1.0, 2.2 / 3.0], [0.4 / 3.5, 0.0] ]
+
+        result = sut.getShadowedSquares(squares, sectors)
+
+        assert result == ['.', '.', 's', '.']
