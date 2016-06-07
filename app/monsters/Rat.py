47652119345c165edf20fce68cc4d1ec7df98bd1
Vasja Volin <vasja.volin@gmail.com>
2016-06-07 17:16:51 -0400

    add some random rat movement

--- app/monsters/Rat.py
+++ app/monsters/Rat.py
@@ -1,4 +1,29 @@
+import app
 from app import Monster
+import random
 
 class Rat(Monster):
-    pass
+    def getAction(self, map):
+        square = self.findANearbySquare(map)
+        return {
+            'name': 'MOVE',
+            'target': self,
+            'to': square,
+        }
+
+    def findANearbySquare(self, map):
+        pos_x, pos_y = self.position
+        map_x, map_y = map['size']
+
+        directions = [[-1, 0], [1, 0], [0, -1], [0, 1]]
+        random.shuffle(directions)
+
+        for d in directions:
+            x = pos_x + d[0]
+            y = pos_y + d[1]
+
+            if x < 0 or y < 0 or x >= map_x or y >= map_y:
+                continue
+
+            if type(map['tiles'][y][x]) == app.Floor:
+                return [x, y]
--- tests/monsters/testRat.py
+++ tests/monsters/testRat.py
@@ -1,4 +1,19 @@
+import app
 from app.monsters import Rat
 
 class TestRat:
-    pass
+    def test_it_should_move_around(self):
+        sut = Rat(position = [0, 0])
+        map = {
+            'tiles': [[ app.Floor(), app.Floor() ]],
+            'size': [2, 1],
+            'objects': [],
+        }
+        action = sut.getAction(map)
+        expected = {
+            'name': 'MOVE',
+            'target': sut,
+            'to': [1, 0],
+        }
+
+        assert action == expected
