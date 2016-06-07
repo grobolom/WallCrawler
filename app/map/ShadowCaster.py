dea206739480da2760504c6ff97c458afe293403
Vasja Volin <vasja.volin@gmail.com>
2016-06-07 16:16:41 -0400

    add comments for the shadow caster

--- app/map/ShadowCaster.py
+++ app/map/ShadowCaster.py
@@ -20,6 +20,8 @@ class ShadowCaster:
         height = len(squares)
         width = len(squares[0])
 
+        # we are using a list of empty squares as our 'shadow mask' - if we
+        # find a square is lit, we will replace it with the real value
         results = [[ ' ' for i in range(width) ] for j in range(height) ]
         results[center[1]][center[0]] = '@'
 
@@ -29,7 +31,6 @@ class ShadowCaster:
         return results
 
     def castShadow(self, col, center, sector, squares, results, octant):
-        print('entering sector: ', sector)
         center_x = center[0]
         center_y = center[1]
 
@@ -51,29 +52,59 @@ class ShadowCaster:
                     continue
 
                 square = squares[real_y][real_x]
+
+                # for proper handling of transitions from blocked to non-, we
+                # want to treat the square before the first as matching the
+                # first square
                 if y == x:
                     last_square = square
 
+                # these slopes can be adjusted to handle calculations using
+                # integer math but also for different vision styles like
+                # diagonal corners
                 nw_slope = (y + 0.5) / (x - 0.5)
                 se_slope = (y - 0.5) / (x + 0.5)
                 ne_slope = (y + 0.5) / (x + 0.5)
 
+                # means the square we are looking at is past the light sector,
+                # so we don't have to keep walking down the column
                 if bottom_slope >= nw_slope:
                     break
 
+                # we haven't reached the sector yet
                 if top_slope <= se_slope:
                     continue
 
                 results[real_y][real_x] = squares[real_y][real_x]
 
+                # blocked to non-blocked - we set the top slope of the existing
+                # sector to the se corner of the previous square, which is the
+                # ne sector of the current one. Now when we keep going further
+                # down the x coordinates with this scan, we will shrink more
+                # and more as we hit more blocked squares
                 if last_square == '#' and square == '.':
                     top_slope = ne_slope
 
+                # if we hit a blocking square from a non-blocking square, we 
+                # start a child scan - but then we continue scanning with
+                # the current one. In this way our scans split into two
+                # when we reach a blocking square in the middle of a column - 
+                # the new scan takes over the top portion and the original scan
+                # continues down the bottom portion
                 if last_square == '.' and square == '#':
                     new_sector = [ top_slope, nw_slope ]
                     self.castShadow(x + 1, center, new_sector, squares, results, octant)
 
                 last_square = square
 
+            # this is important for ending a scan if we ended on a set of
+            # blocking squares. Since we create a child scan when we hit a
+            # blocker, if we hit the bottom of the row without finding any more
+            # open squares we need to end our current scan - the last child scan
+            # will take over for us
+            #
+            # ......... -> ........
+            # @...#.... -> @...#
+            #
             if last_square == '#':
                 break
