d196ad2953ad98de757c294c33491384ec80aae4
Vasja Volin <vasja.volin@gmail.com>
2016-06-06 22:09:15 -0400

    add light square marking and the start of square blocking

--- app/map/ShadowCaster.py
+++ app/map/ShadowCaster.py
@@ -11,8 +11,12 @@ class ShadowCaster:
         height = x
         top_slope = sector[0]
         bottom_slope = sector[1]
+        square = squares[y][x]
 
         for y in range(height, -1, -1):
+            # we care about the top left and bottom right of each square for
+            # checking if it's lit. We can switch to the diagonal technique
+            # by adjusting these values
             nw_slope = (y + 0.5) / (x - 0.5)
             se_slope = (y - 0.5) / (x + 0.5)
 
@@ -22,6 +26,15 @@ class ShadowCaster:
 
             # this square is before the light cone, so keep scanning for it
             if top_slope < se_slope:
+                continue
+
+            # light up the square - for now we mark it, later we'll figure out
+            # how to switch it
+            squares[y][x] = '!'
+
+            # if we're blocked we adjust our sector to the right and scan
+            if square == '#':
+                new_top_slope = se_slope
                 continue
 
             print(x, y, nw_slope, se_slope)
