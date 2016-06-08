ca6bb46b47da5c5404ccf34f182a73762fb5b16b
Vasja Volin <vasja.volin@gmail.com>
2016-06-08 19:00:17 -0400

    fix a test not being inside an actual method

--- tests/map/testMapToTileConverter.py
+++ tests/map/testMapToTileConverter.py
@@ -2,21 +2,23 @@ import app
 from app.map import MapToTileConverter
 
 class TestMapToTileConverter:
-    squares = [
-        list('..#..'),
-        list('.#...'),
-        list('#....'),
-        list('....#'),
-        list('...#.'),
-    ]
-    expected = [
-        [ app.Floor(), app.Floor(), app.Wall(),  app.Floor(), app.Floor() ],
-        [ app.Floor(), app.Wall(),  app.Floor(), app.Floor(), app.Floor() ],
-        [ app.Wall(),  app.Floor(), app.Floor(), app.Floor(), app.Floor() ],
-        [ app.Floor(), app.Floor(), app.Floor(), app.Floor(), app.Wall()  ],
-        [ app.Floor(), app.Floor(), app.Floor(), app.Wall(),  app.Floor() ],
-    ]
-    sut = MapToTileConverter()
-    result = sut.convertToTiles(squares)
+    def test_it_should_convert_tiles(self):
+        squares = [
+            list('..#..'),
+            list('.#...'),
+            list('#....'),
+            list('....#'),
+            list('...#.'),
+        ]
+        expected = [
+            [ app.Floor(), app.Floor(), app.Wall(),  app.Floor(), app.Floor() ],
+            [ app.Floor(), app.Wall(),  app.Floor(), app.Floor(), app.Floor() ],
+            [ app.Wall(),  app.Floor(), app.Floor(), app.Floor(), app.Floor() ],
+            [ app.Floor(), app.Floor(), app.Floor(), app.Floor(), app.Wall()  ],
+            [ app.Floor(), app.Floor(), app.Floor(), app.Wall(),  app.Floor() ],
+        ]
+        sut = MapToTileConverter()
+        result = sut.convertToTiles(squares)
 
-    assert result == expected
+        assert result == expected
+
