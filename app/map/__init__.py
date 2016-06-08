d2f2debdc4985adac84295bc44bbdd4cd8d4de94
Vasja Volin <vasja.volin@gmail.com>
2016-06-08 19:32:52 -0400

    add TileShadowCaster to the module

--- app/map/__init__.py
+++ app/map/__init__.py
@@ -4,4 +4,5 @@ from .MapGenerator import MapGenerator
 from .ValidTilePicker import ValidTilePicker 
 
 from .ShadowCaster import ShadowCaster
+from .TileShadowCaster import TileShadowCaster
 from .MapToTileConverter import MapToTileConverter
