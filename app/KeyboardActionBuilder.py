5085bc015b05eca3dd89114f4d58aff0eda7f5b3
Vasja Volin <vasja.volin@gmail.com>
2016-06-09 18:17:54 -0400

    set up a mapping for future menu actions

--- app/KeyboardActionBuilder.py
+++ app/KeyboardActionBuilder.py
@@ -1,8 +1,12 @@
 class KeyboardActionBuilder:
-
+    # TODO: we can probably separate this into different classes that are
+    # responsible for their own portions of the view. It's less clear that way
+    # where the state transitions are but it keeps each state compartmentalized
     def getAction(self, key, view = 'main'):
         if view == 'main':
             return self.getMapAction(key)
+        if view == 'menu':
+            return self.getMenuAction(key)
         return { 'name': 'NONE' }
 
     def getMapAction(self, key):
