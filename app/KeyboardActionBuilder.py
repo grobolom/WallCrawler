6be93b09c85bb5a54aaf53cc2deb913368769cc7
Vasja Volin <vasja.volin@gmail.com>
2016-06-09 23:31:58 -0400

    shifted key handling from just the keybuild builder to a second handler

--- app/KeyboardActionBuilder.py
+++ app/KeyboardActionBuilder.py
@@ -1,29 +1,36 @@
+from app.keyboard import MoveAndAttackHandler
+
 class KeyboardActionBuilder:
+    def __init__(self, moveAndAttackHandler = None):
+        if not moveAndAttackHandler:
+            moveAndAttackHandler = MoveAndAttackHandler()
+        self.mah = moveAndAttackHandler
+
     # TODO: we can probably separate this into different classes that are
     # responsible for their own portions of the view. It's less clear that way
     # where the state transitions are but it keeps each state compartmentalized
-    def getAction(self, key, view = 'main'):
+    def getAction(self, key, state):
+
+        view = 'main'
+        if 'view' in state:
+            view = state['view']
+
         if view == 'main':
-            return self.getMapAction(key)
+            return self.getMapAction(key, state)
         if view == 'menu':
             return self.getMenuAction(key)
+
         return { 'name': 'NONE' }
 
-    def getMapAction(self, key):
-        if key == 'k':
-            return { 'name': 'MOVE_UP' }
-        if key == 'j':
-            return { 'name': 'MOVE_DOWN' }
-        if key == 'h':
-            return { 'name': 'MOVE_LEFT' }
-        if key == 'l':
-            return { 'name': 'MOVE_RIGHT' }
+    def getMapAction(self, key, state):
         if key == 'KEY_ESCAPE':
             return {
                 'name': 'SWITCH_VIEW',
                 'to': 'menu',
             }
-        return { 'name': 'NO_MOVE' }
+        if key == '':
+            return { 'name': 'NO_MOVE' }
+        return self.mah.getAction(key, state)
 
     def getMenuAction(self, key):
         if key == 'KEY_ESCAPE':
--- tests/testKeyboardActionBuilder.py
+++ tests/testKeyboardActionBuilder.py
@@ -1,46 +1,35 @@
+from mock import MagicMock as Mock
 from app import KeyboardActionBuilder
 
 class TestKeyboardActionBuilder:
-    def test_it_should_map_j_to_move_down(self):
+    def test_it_should_delegate_movement_to_another_handler(self):
         key = 'j'
-        k = KeyboardActionBuilder()
+        state = {}
+        handler = Mock()
+        sut = KeyboardActionBuilder(handler)
+        sut.getAction(key, state)
 
-        assert k.getAction(key) == { 'name': 'MOVE_DOWN' }
-    def test_it_should_map_k_to_move_up(self):
-        key = 'k'
-        k = KeyboardActionBuilder()
-
-        assert k.getAction(key) == { 'name': 'MOVE_UP' }
-    def test_it_should_map_l_to_move_right(self):
-        key = 'l'
-        k = KeyboardActionBuilder()
-
-        assert k.getAction(key) == { 'name': 'MOVE_RIGHT' }
-    def test_it_should_map_h_to_move_left(self):
-        key = 'h'
-        k = KeyboardActionBuilder()
-
-        assert k.getAction(key) == { 'name': 'MOVE_LEFT' }
+        handler.getAction.assert_called_once_with(key, state)
 
     def test_it_should_move_other_keys_to_nothing(self):
         key = ''
         k = KeyboardActionBuilder()
 
-        assert k.getAction(key) == { 'name': 'NO_MOVE' }
+        assert k.getAction(key, {}) == { 'name': 'NO_MOVE' }
 
     def test_it_should_open_the_menu_on_escape(self):
         key = 'KEY_ESCAPE'
         k = KeyboardActionBuilder()
-        assert k.getAction(key) == {
+        assert k.getAction(key, {}) == {
             'name': 'SWITCH_VIEW',
             'to': 'menu',
         }
 
     def test_it_should_leave_the_menu_on_escape(self):
         key = 'KEY_ESCAPE'
-        view = 'menu'
+        state = { 'view': 'menu' }
         k = KeyboardActionBuilder()
-        assert k.getAction(key, view) == {
+        assert k.getAction(key, state) == {
             'name': 'SWITCH_VIEW',
             'to': 'main',
         }
