1e9d6ddb5f263a87a75e1f68017bc698914ceedb
Vasja Volin <vasja.volin@gmail.com>
2016-06-10 22:57:59 -0400

    add some code to handle drawing views

--- app/utils/ViewRenderer.py
+++ app/utils/ViewRenderer.py
@@ -1,2 +1,8 @@
 class ViewRenderer:
-    pass
+    def __init__(self, views):
+        self.views = views
+
+
+    def draw(self, state):
+        view_to_draw = state['view']
+        return self.views[view_to_draw].draw(state)
--- tests/utils/testViewRenderer.py
+++ tests/utils/testViewRenderer.py
@@ -1,4 +1,13 @@
+from mock import MagicMock as Mock
 from app.utils import ViewRenderer
 
-class TestViewRenderer(self):
-    pass
+class TestViewRenderer:
+    def test_it_should_draw_a_view(self):
+        mockView = Mock()
+        sut = ViewRenderer({
+            'main': mockView
+        })
+        state = { 'view': 'main' }
+        sut.draw(state)
+
+        mockView.draw.assert_called_once_with(state)
