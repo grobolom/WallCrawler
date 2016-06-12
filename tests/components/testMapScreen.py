f0d86dec9692e7555f4a36c43968557dab61e852
Vasja Volin <vasja.volin@gmail.com>
2016-06-12 18:46:44 -0400

    added a test but not sure of api yet

--- tests/components/testMapScreen.py
+++ tests/components/testMapScreen.py
@@ -1,4 +1,11 @@
 from app import MapScreen
 
 class TestMapScreen:
-    pass
+    def test_it_should_use_the_screen_to_draw_a_map(self):
+        state = {
+            'settings': {
+                'screen_size': [21, 13]
+            }
+        }
+        sut = MapScreen()
+        new_state = sut.render(state)
