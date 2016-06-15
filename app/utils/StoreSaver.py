07686294242bc773f9765fb102824518be714d1e
Vasja Volin <vasja.volin@gmail.com>
2016-06-14 21:17:25 -0400

    add a store saver to save and load state

--- app/utils/StoreSaver.py
+++ app/utils/StoreSaver.py
@@ -1,2 +1,13 @@
+import jsonpickle
+
 class StoreSaver:
-    pass
+    def save(state):
+        to_save = jsonpickle.encode(state)
+        with open('../saves/store.json', 'w') as file:
+            file.write(to_save)
+
+    def load():
+        state = '{}'
+        with open('../saves/store.json', 'r') as file:
+            state = file.read()
+        return jsonpickle.decode(state)
